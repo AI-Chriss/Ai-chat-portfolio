f46ec380778101b038d9f5ac23bc2a942ae49336
Chriss <lewandowski.krzysztof433@gmail.com>
2025-03-10 11:11:26 +0000

    added tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   server: {
     host: '0.0.0.0',
+    allowedHosts: ['cc1da68f-73c6-4a55-bc1b-a6046adcf685-00-28hl9isojqt11.janeway.replit.dev'],
   }
 })
